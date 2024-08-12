55409a3a4840b13fb1772c539ea699317ca1153b
Abishan Bhavananthan <abishan@Abishans-MacBook-Air.local>
2024-08-12 19:56:44 -0400

    convert Tracks.js, Artists.js to rfc

--- src/ArtistsPage/Artists.js
+++ src/ArtistsPage/Artists.js
@@ -1,64 +1,64 @@
-import React, { Component } from 'react'
-import TimeRangeSelector from '../components/TimeRangeSelector.js'
-import {getTopItems} from '../apiCalls.js'
+import React, { useState, useEffect } from 'react';
+import TimeRangeSelector from '../components/TimeRangeSelector.js';
+import { getTopItems } from '../apiCalls.js';
 import { ErrorBoundary } from '../ErrorBoundary.js';
-import RenderArtists from "./RenderArtists.js"
+import RenderArtists from "./RenderArtists.js";
 
-export default class Artists extends Component {
-  constructor(props) {
-    super(props); 
-  
-    this.state = {
-      top_artists:{
-        "short_term":[],
-        "medium_term":[],
-        "long_term":[]
-      },
-      error: false
-    } 
-  }
+export default function Artists(props) {
+  const [topArtists, setTopArtists] = useState({
+    short_term: [],
+    medium_term: [],
+    long_term: [],
+  });
+  const [error, setError] = useState(false);
 
-  componentDidMount(){//on page/component load
-    if (this.state.top_artists["short_term"].length!==0){return}//do not call api repeadtly
-    if (this.props.topArtists === null){//if artists havent already been loaded 
-      this.onGetTopArtists();
+  useEffect(function() {//on component load
+    if (props.topArtists === null) {//if artists havent been loaded yet
+      onGetTopArtists();
       return
-    }
-    //if artists already loaded, use top artists passed from parent compoenet
-    this.setState({top_artists:this.props.topArtists})
-  }
-  
-  onGetTopArtists = () => {
-    var that = this;//cannot access this inside promise
-    const artistPromises = []
-    artistPromises.push(getTopItems(this.props.token, "artists","short_term"))
-    artistPromises.push(getTopItems(this.props.token, "artists","medium_term"))
-    artistPromises.push(getTopItems(this.props.token, "artists","long_term"))
-    
-    Promise.all(artistPromises).then((topArtistsResults) => {
-      console.log("re")
-      let updatedTopArtists= {
-        "short_term":topArtistsResults[0].items,
-        "medium_term":topArtistsResults[1].items, 
-        "long_term":topArtistsResults[2].items
+    } 
+    //if already loaded update state with tracks passed from parent component
+    setTopArtists(props.topArtists);
+    // eslint-disable-next-line
+  }, []);
+
+  function onGetTopArtists() {
+    const artistPromises = [
+      getTopItems(props.token, 'artists', 'short_term'),
+      getTopItems(props.token, 'artists', 'medium_term'),
+      getTopItems(props.token, 'artists', 'long_term'),
+    ];
+
+    Promise.all(artistPromises).then(function(topArtistsResults) {
+      if (topArtistsResults[0]===false || topArtistsResults[1]===false ||  topArtistsResults[2]===false){
+        setError(true)
+        return
       }
-      that.setState({
-        top_artists:updatedTopArtists
-      })
-      that.props.updateTopArtistsFunc(updatedTopArtists)//stores data in parent component to avoid multiple fetches on rerender
+      const updatedTopArtists = {
+        short_term: topArtistsResults[0].items,
+        medium_term: topArtistsResults[1].items,
+        long_term: topArtistsResults[2].items,
+      };
+      setTopArtists(updatedTopArtists);
+      props.updateTopArtistsFunc(updatedTopArtists); // Store data in parent component to avoid multiple fetches on rerender
     })
+    .catch(function() {
+      setError(true);
+    });
   }
 
-  render() {
-    if (this.state.error){throw new Error("Can't fetch tracks")}
-    return (
+  if (error) {throw new Error("Failed to fetch artists");}
+  return (
     <React.Fragment>
       <h1 className="page-title">Your Top Artists</h1>
       <ErrorBoundary fallback="TimeRange.js">
-        <TimeRangeSelector topItemsComponent={(timeRange) => <RenderArtists artists={this.state.top_artists[timeRange]} />}/> 
+        <TimeRangeSelector
+          topItemsComponent={function(timeRange) {
+            return <RenderArtists artists={topArtists[timeRange]} />;
+          }}
+        />
       </ErrorBoundary>
-      
     </React.Fragment>
-    )
-  }
+  );
 }
+
--- src/TracksPage/Tracks.js
+++ src/TracksPage/Tracks.js
@@ -1,67 +1,59 @@
-import React, { Component } from 'react'
-import TimeRangeSelector from '../components/TimeRangeSelector.js'
-import {getTopItems} from '../apiCalls.js'
+import React, { useState, useEffect } from 'react';
+import TimeRangeSelector from '../components/TimeRangeSelector.js';
+import { getTopItems } from '../apiCalls.js';
 import { ErrorBoundary } from '../ErrorBoundary.js';
 import RenderTracks from './RenderTracks.js';
-export default class Tracks extends Component {
-  constructor(props) {
-      super(props);
-    
-      this.state = { 
-        top_tracks:{
-          "short_term":[],
-          "medium_term":[], 
-          "long_term":[]
-        }, 
-        error:false
-      }
-      //bind event handler
-      this.onGetTopTracks = this.onGetTopTracks.bind(this)
-  }
 
-  componentDidMount(){//on page/component load
-    if (this.state.top_tracks["short_term"].length!==0){return}//do not call api repeadtly
-    if (this.props.topTracks === null){//if tracks havent been loaded yet
-      this.onGetTopTracks();
+export default function Tracks(props){
+  const [topTracks, setTopTracks] = useState({
+    short_term: [],
+    medium_term: [],
+    long_term: [],
+  });
+  const [error, setError] = useState(false);
+
+  useEffect(() => {//on component load
+    if (props.topTracks === null) {//if tracks havent been loaded yet
+      onGetTopTracks();
       return
-    }
-    //if tracks already loaded, use top tracks passed from parent compoenet
-    this.setState({top_tracks:this.props.topTracks})
-  }
+    } 
+    //if already loaded update state with tracks passed from parent component
+    setTopTracks(props.topTracks);
+    // eslint-disable-next-line
+  }, []);
 
-  onGetTopTracks = () =>{
-    var that = this;//cannot access this inside promise
-    const trackPromises = []
-    trackPromises.push(getTopItems(this.props.token, "tracks","short_term"))
-    trackPromises.push(getTopItems(this.props.token, "tracks","medium_term"))
-    trackPromises.push(getTopItems(this.props.token, "tracks","long_term"))
-    
+  const onGetTopTracks = () => {
+    const trackPromises = [
+      getTopItems(props.token, 'tracks', 'short_term'),
+      getTopItems(props.token, 'tracks', 'medium_term'),
+      getTopItems(props.token, 'tracks', 'long_term'),
+    ];
+
     Promise.all(trackPromises).then((topTracksResults) => {
-      let updatedTopTracks= {
-        "short_term":topTracksResults[0].items,
-        "medium_term":topTracksResults[1].items, 
-        "long_term":topTracksResults[2].items
+      if (topTracksResults[0]===false || topTracksResults[1]===false ||  topTracksResults[2]===false){
+        setError(true)
+        return
       }
-      that.setState({
-        top_tracks:updatedTopTracks
-      })
-      that.props.updateTopTracksFunc(updatedTopTracks)//stores data in parent compoent to avoid multiple fetches on rerender
+      const updatedTopTracks = {
+        short_term: topTracksResults[0].items,
+        medium_term: topTracksResults[1].items,
+        long_term: topTracksResults[2].items,
+      };
+      setTopTracks(updatedTopTracks);
+      props.updateTopTracksFunc(updatedTopTracks); // Store data in parent component to avoid multiple fetches on rerender
     })
-  }
+      .catch(() => setError(true));
+  };
 
-  render() {
-    if (this.state.error){throw new Error("Can't fetch tracks")}
+  if (error) {throw new Error("Failed to fetch tracks");}
 
-    return (
-      <React.Fragment>
-        <h1 className="page-title">Your Top Tracks</h1>
-        <ErrorBoundary fallback="TimeRange.js">
-          <TimeRangeSelector topItemsComponent={(timeRange) => <RenderTracks tracks={this.state.top_tracks[timeRange]} />}/>
-        </ErrorBoundary>
-
-        
-      </React.Fragment>
-  )
-  }
-}
+  return (
+    <React.Fragment>
+      <h1 className="page-title">Your Top Tracks</h1>
+      <ErrorBoundary fallback="TimeRange.js">
+        <TimeRangeSelector topItemsComponent={(timeRange) => <RenderTracks tracks={topTracks[timeRange]} />}/>
+      </ErrorBoundary>
+    </React.Fragment>
+  );
+};
 
