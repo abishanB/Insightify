5342f44cd28c1001f53dc8436c30d6b71b24efd1
abishanB <abishan.bhavan@gmail.com>
2025-03-09 19:20:04 -0400

    home page resizing fix

--- client/src/PlaylistsPage/GenreChart.js
+++ client/src/PlaylistsPage/GenreChart.js
@@ -3,7 +3,7 @@ import { Doughnut } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import LoadingIcon from "../components/LoadingIcon";
-
+import "./styles/GenreChart.css"
 Chart.register(CategoryScale);
 
 function calculateGenreComposition(data) {
